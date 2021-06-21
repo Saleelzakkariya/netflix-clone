889cc6fd561169ef5fdb8dba4c1066a0b1e169f3
Mohammed saleel <saleelzakkariya@gmail.com>
2021-06-21 18:04:43 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/NavBar/NavBar'
+import Banner from './components/banner/banner'
+import RowPost from './components/RowPost/RowPost';
+import {actions,orginals,romantic,horror} from './URLS/url'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar></Navbar>
+     <Banner></Banner>
+     <RowPost url={orginals} title='Films' isSmall={false}/>
+     <RowPost url={actions} title='Action' isSmall={true}/>
+     <RowPost url={romantic} title='Romatic Movies' isSmall={true}/>
+     <RowPost url={horror} title='Horror Movies' isSmall={true}/>
     </div>
   );
 }
